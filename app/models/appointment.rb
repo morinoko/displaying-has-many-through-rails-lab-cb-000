5332569afcc0648dddb3f415139200fd9b9a97fe
Felice Forby <feliceforby@gmail.com>
2018-10-17 22:51:59 +0000

    automatically backed up by learn

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -2,6 +2,14 @@ class Appointment < ActiveRecord::Base
   belongs_to :doctor
   belongs_to :patient
 
+  def date
+    self.appointment_datetime.strftime("%B %-d, %Y")
+  end
+
+  def time
+    self.appointment_datetime.strftime(""%k:%M")
+  end
+  
   def doctor_name
     self.doctor.name
   end
